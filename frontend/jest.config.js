c6dd868ce998a5e7126fe4c136b4a69c0ae44e45
tndhk <baggio0707@yahoo.co.jp>
2025-04-01 13:28:57 +0900

    Create jest.config.js

---
+++ frontend/jest.config.js
@@ -0,0 +1,26 @@
+// jest.config.js
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // next.config.jsとテスト環境用の.envファイルが配置されたディレクトリをセット
+  dir: './',
+});
+
+// Jestのカスタム設定を設置する場所
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/_*.{js,jsx,ts,tsx}',
+    '!src/**/*.stories.{js,jsx,ts,tsx}',
+  ],
+};
+
+// createJestConfigを定義することによって、上記2つの設定がマージされる
+module.exports = createJestConfig(customJestConfig);
